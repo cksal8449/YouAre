0a704802bc1ccb5dfccadff422efd9929cbad6e9
Janeee205 <ga473625@gmail.com>
2023-12-01 15:48:36 +0900

    💡 주석추가

--- express/index.js
+++ express/index.js
@@ -1,32 +1,34 @@
-const express = require('express');
+// http://localhost:7000/
+
+const express = require("express");
 const app = express();
 
 app.listen(7000, function () {
-    console.log('7000번 포트')
-})
+  console.log("7000번 포트");
+});
 
-app.get('', function (requests, response) {
-    response.sendFile(__dirname + '/home.html')
-})
+app.get("", function (requests, response) {
+  response.sendFile(__dirname + "/home.html");
+});
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 
-app.get('/product_perfume', function (requests, response) {
-    response.sendFile(__dirname + '/product_perfume/product_perfume.html')
-})
+app.get("/product_perfume", function (requests, response) {
+  response.sendFile(__dirname + "/product_perfume/product_perfume.html");
+});
 
-app.get('/perduct_candle', function (requests, response) {
-    response.sendFile(__dirname + '/perduct_candle/perduct_candle.html')
-})
+app.get("/perduct_candle", function (requests, response) {
+  response.sendFile(__dirname + "/perduct_candle/perduct_candle.html");
+});
 
-app.get('/login', function (requests, response) {
-    response.sendFile(__dirname + '/login/login.html')
-})
+app.get("/login", function (requests, response) {
+  response.sendFile(__dirname + "/login/login.html");
+});
 
-app.get('/signup', function (requests, response) {
-    response.sendFile(__dirname + '/signup/signup.html')
-})
+app.get("/signup", function (requests, response) {
+  response.sendFile(__dirname + "/signup/signup.html");
+});
 
-app.get('/product_detail', function (requests, response) {
-    response.sendFile(__dirname + '/product_detail/product_detail.html')
-})
+app.get("/product_detail", function (requests, response) {
+  response.sendFile(__dirname + "/product_detail/product_detail.html");
+});
