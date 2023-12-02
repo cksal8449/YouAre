d2412955477d6c8fc19dbc4a3fd779c4603202e7
Janeee205 <ga473625@gmail.com>
2023-12-02 23:01:04 +0900

    🐛 자잘한 수정

--- express/index.js
+++ express/index.js
@@ -17,8 +17,8 @@ app.get("/product_perfume", function (requests, response) {
   response.sendFile(__dirname + "/product_perfume/product_perfume.html");
 });
 
-app.get("/perduct_candle", function (requests, response) {
-  response.sendFile(__dirname + "/perduct_candle/perduct_candle.html");
+app.get("/product_perfume", function (requests, response) {
+  response.sendFile(__dirname + "/product_perfume/product_perfume.html");
 });
 
 app.get("/login", function (requests, response) {
