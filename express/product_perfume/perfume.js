680914a6def5d4e59b87448a4c640225a7413d78
Janeee205 <ga473625@gmail.com>
2023-12-02 22:46:12 +0900

    ♻️ 반응형 수정

--- express/product_perfume/perfume.js
+++ express/product_perfume/perfume.js
@@ -1,120 +1,120 @@
 // header
-$('.sub').hide();
-$('.gnb > li').hover(function () {
-
-  $(this).find('ul').stop().slideDown(500);
-  $(this).find('a').addClass('on');
-
-}, function () {
-  $(this).find('ul').stop().slideUp(500);
-  $(this).find('a').removeClass('on');
-})
+$(".sub").hide();
+$(".gnb > li").hover(
+  function () {
+    $(this).find("ul").stop().slideDown(500);
+    $(this).find("a").addClass("on");
+  },
+  function () {
+    $(this).find("ul").stop().slideUp(500);
+    $(this).find("a").removeClass("on");
+  }
+);
 
 // sidebar 보이기
-$('.sidebar-btn a').on('click', function (e) {
+$(".sidebar-btn a").on("click", function (e) {
   e.preventDefault();
-  $('.sidebar').addClass('active');
-})
+  $(".sidebar").addClass("active");
+});
 
-$('.sidebar-overlay, .sidemenu #close-btn').on('click', function () {
-  $('.sidebar').removeClass('active');
-})
+$(".sidebar-overlay, .sidemenu #close-btn").on("click", function () {
+  $(".sidebar").removeClass("active");
+});
 
 // product
 let product = [
   {
-    link: '',
-    img: '../images/Perfume_slide1-1.png',
-    title: 'Aeternum',
-    note: 'Thick chamomile | Soft wood grain | Musk',
-    ml: '100',
-    price: '113,000'
+    link: "",
+    img: "../images/Perfume_slide1-1.png",
+    title: "Aeternum",
+    note: "Thick chamomile | Soft wood grain | Musk",
+    ml: "100",
+    price: "113,000",
   },
   {
-    link: '',
-    img: '../images/Perfume_slide1-2.png',
-    title: 'Animi',
-    note: 'Bergamot | Green tangerine | Sandalwood',
-    ml: '100',
-    price: '113,000'
+    link: "",
+    img: "../images/Perfume_slide1-2.png",
+    title: "Animi",
+    note: "Bergamot | Green tangerine | Sandalwood",
+    ml: "100",
+    price: "113,000",
   },
   {
-    link: '',
-    img: '../images/Perfume_slide1-3.png',
-    title: 'Angelus',
-    note: 'Warm air | Orange peel | Musk',
-    ml: '100',
-    price: '113,000'
+    link: "",
+    img: "../images/Perfume_slide1-3.png",
+    title: "Angelus",
+    note: "Warm air | Orange peel | Musk",
+    ml: "100",
+    price: "113,000",
   },
   {
-    link: '',
-    img: '../images/Perfume_slide1-4.png',
-    title: 'Beatrice',
-    note: 'Marshmallow | Orange blossom | Tobacco',
-    ml: '100',
-    price: '113,000'
+    link: "",
+    img: "../images/Perfume_slide1-4.png",
+    title: "Beatrice",
+    note: "Marshmallow | Orange blossom | Tobacco",
+    ml: "100",
+    price: "113,000",
   },
   {
-    link: '',
-    img: '../images/Perfume_slide1-5.png',
-    title: 'Benedictum',
-    note: 'Cool pear | Suede | Bittersweet Galbanum',
-    ml: '100',
-    price: '113,000'
+    link: "",
+    img: "../images/Perfume_slide1-5.png",
+    title: "Benedictum",
+    note: "Cool pear | Suede | Bittersweet Galbanum",
+    ml: "100",
+    price: "113,000",
   },
   {
-    link: '',
-    img: '../images/Perfume_slide1-6.png',
-    title: 'Bellatrix',
-    note: 'White champagne | Darjeeling tea | Creamy musk',
-    ml: '100',
-    price: '113,000'
+    link: "",
+    img: "../images/Perfume_slide1-6.png",
+    title: "Bellatrix",
+    note: "White champagne | Darjeeling tea | Creamy musk",
+    ml: "100",
+    price: "113,000",
   },
   {
-    link: '',
-    img: '../images/Perfume_slide1-7.png',
-    title: 'Cordelia',
-    note: 'Haystack | Thick-scented rum | Patchouli',
-    ml: '100',
-    price: '113,000'
+    link: "",
+    img: "../images/Perfume_slide1-7.png",
+    title: "Cordelia",
+    note: "Haystack | Thick-scented rum | Patchouli",
+    ml: "100",
+    price: "113,000",
   },
   {
-    link: '',
-    img: '../images/Perfume_slide1-8.png',
-    title: 'Caligo',
-    note: 'Green apple with dew | White tulip | Sandalwood',
-    ml: '100',
-    price: '113,000'
+    link: "",
+    img: "../images/Perfume_slide1-8.png",
+    title: "Caligo",
+    note: "Green apple with dew | White tulip | Sandalwood",
+    ml: "100",
+    price: "113,000",
   },
   {
-    link: '',
-    img: '../images/Perfume_slide1-9.png',
-    title: 'Fottuna',
-    note: 'Woody spicy | Dense fog | Thick leafy shade',
-    ml: '100',
-    price: '113,000'
+    link: "",
+    img: "../images/Perfume_slide1-9.png",
+    title: "Fottuna",
+    note: "Woody spicy | Dense fog | Thick leafy shade",
+    ml: "100",
+    price: "113,000",
   },
-]
+];
 
-let boxItem = document.querySelectorAll('box-item');
+let boxItem = document.querySelectorAll("box-item");
 boxItem.forEach(function (item, length) {
-  item.querySelector('link').src = products[length].link;
-  item.querySelector('img').src = products[length].img;
-  item.querySelector('.per-info h2').innerHTML = product[length].title;
-  item.querySelectorAll('.per-info p').innerHTML = product[length].note;
-  item.querySelectorAll('.per-info p').innerHTML = product[length].ml;
-  item.querySelectorAll('.per-info p').innerHTML = product[length].price;
-})
-
+  item.querySelector("link").src = products[length].link;
+  item.querySelector("img").src = products[length].img;
+  item.querySelector(".per-info h2").innerHTML = product[length].title;
+  item.querySelectorAll(".per-info p").innerHTML = product[length].note;
+  item.querySelectorAll(".per-info p").innerHTML = product[length].ml;
+  item.querySelectorAll(".per-info p").innerHTML = product[length].price;
+});
 
 product.forEach(function (item, index) {
   let boxItem = `
   <div class='box-item'>
-  <div class='item-img-box'>
-  <a href='${item.link}'>
-   <img src='${item.img}' alt='${item.title}'>
-   </a> 
-  </div>
+    <div class='item-img-box'>
+      <a href='${item.link}'>
+      <img src='${item.img}' alt='${item.title}'>
+      </a> 
+    </div>
     <div class='per-info'>
      <a href='${item.link}'>
       <h2>${item.title}</h2>
@@ -127,8 +127,8 @@ product.forEach(function (item, index) {
       <button class='cart-btn'>Add to cart</button>
     </div>
   </div>
-  `
-  document.querySelector('.box-list').insertAdjacentHTML('beforeend', boxItem);
-})
+  `;
+  document.querySelector(".box-list").insertAdjacentHTML("beforeend", boxItem);
+});
 
-console.log(123)
+console.log(123);
